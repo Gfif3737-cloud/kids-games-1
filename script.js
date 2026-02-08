a2c430364d1f6714208a07ab6699b7bacf5026a0
Gfif3737-cloud <Gfif3737@icloud.com>
2026-02-08 15:34:45 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const game = document.getElementById("game");
+
+function showPairs() {
+  game.innerHTML = `
+    <h2>🧠 Игра "Найди пару"</h2>
+    <p>Нажми на карточки:</p>
+    <div style="font-size:40px">
+      🐶 🐱 🐶 🐱
+    </div>
+  `;
+}
+
+function showMaze() {
+  game.innerHTML = `
+    <h2>🧩 Лабиринт</h2>
+    <p>Скоро здесь будет игра 😉</p>
+  `;
+}
